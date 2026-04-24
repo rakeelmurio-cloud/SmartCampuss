cd4d8850b4a6e3a0722d736e211e4865d9e682b2
Lithusha <lithupatrick@gmail.com>
2026-04-24 10:46:39 +0530

    update resource error

--- src/main/java/com/smcsystem/smart_campus_system/dto/request/UpdateResourceRequest.java
+++ src/main/java/com/smcsystem/smart_campus_system/dto/request/UpdateResourceRequest.java
@@ -8,14 +8,14 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class UpdateResourceRequest {
-    private String name;
-    private ResourceType type;
+// public class UpdateResourceRequest {
+//     private String name;
+//     private ResourceType type;
 
-    @Min(value = 1, message = "Capacity must be at least 1")
-    private Integer capacity;
-    private String location;
-    private String description;
-    private List<AvailabilityWindowRequest> availability;
-    private ResourceStatus status;
-}
+//     @Min(value = 1, message = "Capacity must be at least 1")
+//     private Integer capacity;
+//     private String location;
+//     private String description;
+//     private List<AvailabilityWindowRequest> availability;
+//     private ResourceStatus status;
+// }

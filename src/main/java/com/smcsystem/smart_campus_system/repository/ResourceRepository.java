6d838e27a2c87726acacde7d45d23dcff51a800c
Lithusha <lithupatrick@gmail.com>
2026-04-24 10:54:50 +0530

    resource repository error

--- src/main/java/com/smcsystem/smart_campus_system/repository/ResourceRepository.java
+++ src/main/java/com/smcsystem/smart_campus_system/repository/ResourceRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-// public interface ResourceRepository extends MongoRepository<Resource, String> {
-//     List<Resource> findByType(ResourceType type);
-//     List<Resource> findByStatus(ResourceStatus status);
-// }
+public interface ResourceRepository extends MongoRepository<Resource, String> {
+    List<Resource> findByType(ResourceType type);
+    // List<Resource> findByStatus(ResourceStatus status);
+}
